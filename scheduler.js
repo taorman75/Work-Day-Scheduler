92c9dc3c2fdc7620d6e26e8130416109937bc597
Queen of Corgis <tana.owens@gmail.com>
2020-07-04 20:11:49 -0600

    fixed css

--- scheduler.js
+++ scheduler.js
@@ -5,10 +5,14 @@ console.log(currDate);
 var now = moment();
 console.log(now);
 
-var i = 18;
+var i = 20;
 
-if ((moment().hour(i)) <= now) {
+if ((moment().hour(i)) < now) {
+    $(".form-control").addClass("past");
     console.log("earlier");
-} else {
+} else if ((moment().hour(i)) > now) {
+    $(".form-control").addClass("future");
     console.log("later");  
+} else {
+    $(".form-control").addClass("present");
 }
