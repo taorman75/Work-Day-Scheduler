98f5db5850ac15c0da2cabf581395c52cb9cd8e2
Queen of Corgis <tana.owens@gmail.com>
2020-07-04 18:52:09 -0600

    testing time recognition

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,14 @@
+var currDate = moment().format('dddd MMMM Do, YYYY');
+$("#currentDay").text(currDate);
+console.log(currDate);
+
 var now = moment();
-$("#currentDay").text(now);
 console.log(now);
+
+var i = 18;
+
+if ((moment().hour(i)) <= now) {
+    console.log("earlier");
+} else {
+    console.log("later");  
+}
