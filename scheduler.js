b6d961769c3b9278a5182f7f95b6abc7e2285471
Queen of Corgis <tana.owens@gmail.com>
2020-07-07 16:13:41 -0600

    fixed local storage stability/repopulation

--- scheduler.js
+++ scheduler.js
@@ -37,10 +37,15 @@ localStorage.setItem(id, toDo);
 console.log("clicked");
 });
 
-
-$(".form-control").each(function(element){
-  localStorage.getItem(toDo);
-  console.log(toDo);
-  $("todo-" + id).text(toDo); 
-})
-
+$("#todo-8").val(localStorage.getItem("8"));
+$("#todo-9").val(localStorage.getItem("9"));
+$("#todo-10").val(localStorage.getItem("10"));
+$("#todo-11").val(localStorage.getItem("11"));
+$("#todo-12").val(localStorage.getItem("12"));
+$("#todo-13").val(localStorage.getItem("13"));
+$("#todo-14").val(localStorage.getItem("14"));
+$("#todo-15").val(localStorage.getItem("15"));
+$("#todo-16").val(localStorage.getItem("16"));
+$("#todo-17").val(localStorage.getItem("17"));
+$("#todo-18").val(localStorage.getItem("18"));
+$("#todo-19").val(localStorage.getItem("19"));
